6559d0dc2b1188ab9c1547ea9f1d4424713e0b88
Trịnh Quốc Bảo <21521867@gm.uit.edu.vn>
2024-04-05 03:04:47 +0700

    Rename files extension

--- libs/Redux/store.ts
+++ libs/Redux/store.ts
@@ -3,7 +3,6 @@ import { persistStore, persistReducer, FLUSH, REHYDRATE, REGISTER, PAUSE, PERSIS
 import createWebStorage from "redux-persist/es/storage/createWebStorage";
 import player from "./features/slices/player";
 import queue from "./features/slices/queue";
-import user from "./features/slices/user";
 import { songsAPI } from "./features/apiSlices/songs";
 import { albumsAPI } from "./features/apiSlices/albums";
 import { playlistsAPI } from "./features/apiSlices/playlists";
@@ -14,7 +13,6 @@ import { usersAPI } from "./features/apiSlices/users";
 const rootReducer = combineReducers({
     player,
     queue,
-    user,
     [songsAPI.reducerPath]: songsAPI.reducer,
     [albumsAPI.reducerPath]: albumsAPI.reducer,
     [playlistsAPI.reducerPath]: playlistsAPI.reducer,
